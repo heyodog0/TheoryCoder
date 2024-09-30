9ca5a63fc45f4b47ddfcd81d24703a5cb0265539
Zergham Ahmed <43177006+ZerghamAhmed@users.noreply.github.com>
2024-09-30 11:54:09 -0400

    learned transformation

--- prompts/wm_response_lv5.py
+++ prompts/wm_response_lv5.py
@@ -1,13 +1,13 @@
-# make sure to include these import statements
 from predicates import *
 from copy import deepcopy
 from games import BabaIsYou
 from babareport import BabaReportUpdater
 from utils import directions
 
+# Function to update pushable entity's coordinates
 def make_push(state, pushable, old_coords, delta):
     if pushable in state:
-        pushable_new_coords = [old_coords[0] + delta[0], old_coords[1] + delta[1]]
+        pushable_new_coords = [old_coords[0] + delta[0], old_coords[1] + delta[1]] 
         index = state[pushable].index(old_coords)
         state[pushable][index] = pushable_new_coords
         print(f"PUSHED {pushable} from {old_coords} to {pushable_new_coords}")
@@ -19,9 +19,18 @@ def simulate_movement(new_coordinates, delta, grid_size=10):
         listofcoords.append(next_coords)
     return listofcoords
 
+# make sure to include these import statements
+from predicates import *
+from copy import deepcopy
+from games import BabaIsYou
+from babareport import BabaReportUpdater
+from utils import directions
+
 def transition_model(state, action):
-    transformations = {}
-    object_words = {entity for entity in state if entity.endswith('_word') and entity not in ['is_word', 'push_word', 'you_word', 'win_word', 'stop_word', 'sink_word']}
+    # Apply object transformation rules
+    transformations = {}  # Mapping from old_obj to new_obj
+
+    object_words = {entity for entity in state if entity.endswith('_word') and entity not in ['is_word', 'push_word', 'you_word', 'win_word', 'stop_word']}
 
     for obj_word1 in object_words:
         for obj_word2 in object_words:
@@ -29,8 +38,11 @@ def transition_model(state, action):
                 if rule_formed(state, obj_word1, 'is_word', obj_word2):
                     old_obj = obj_word1[:-5] + '_obj'
                     new_obj = obj_word2[:-5] + '_obj'
+                    # breakpoint()
+
                     transformations[old_obj] = new_obj
 
+    # Apply the transformations
     entities_to_remove = set()
     entities_to_add = {}
 
@@ -42,20 +54,24 @@ def transition_model(state, action):
                 entities_to_add[new_obj] = []
             entities_to_add[new_obj].extend(coords)
 
+    # Remove the old objects
     for entity in entities_to_remove:
         del state[entity]
 
+    # Add the new objects
     for entity, coords in entities_to_add.items():
         if entity not in state:
             state[entity] = []
         state[entity].extend(coords)
 
+    # Determine controllable entities (objects corresponding to "is you" rule)
     controllables = {
         entity for entity in state
         if rule_formed(state, f'{entity[:-4]}_word', 'is_word', 'you_word')
     }
     print("controllables", controllables)
 
+    # Determine initial pushable entities
     pushables = {
         entity for entity in state
         if (
@@ -65,6 +81,7 @@ def transition_model(state, action):
     }
     print("initial pushables", pushables)
 
+    # Determine 'stop' entities (objects corresponding to "is stop" rule)
     stop_entities = set()
     for entity in state:
         if entity.endswith('_obj'):
@@ -73,6 +90,7 @@ def transition_model(state, action):
                 stop_entities.add(entity)
     print("stop entities", stop_entities)
 
+    # Build obstacle_coords from 'stop' entities
     obstacle_coords = set()
     for entity in stop_entities:
         for coord in state[entity]:
@@ -85,62 +103,76 @@ def transition_model(state, action):
             pushables_coords.add(tuple(coord))
 
     empties_coords = set()
-    if 'empty' in state:
-        for coord in state['empty']:
-            empties_coords.add(tuple(coord))
+    for coord in state["empty"]:
+        empties_coords.add(tuple(coord))
 
-    state_before_action = deepcopy(state)
-
+    # get controllable entity
     for obj, coords in state.items():
         if obj.endswith('_obj'):
             obj_name = obj[:-4]
             if obj in controllables:
+
                 print(f"Controlling {obj_name}_obj")
 
+                # turns action into coordinate change for state dict
                 if action in directions:
                     delta = directions[action]
 
+                    # need to move all controlled entities in a given direction
                     for i, coord in enumerate(coords):
                         print("printing coords", coord)
+
+                        # calculate new coordinates after move
                         new_coords = [coord[0] + delta[0], coord[1] + delta[1]]
 
                         if tuple(new_coords) in obstacle_coords:
+                            # Cannot move into a 'Stop' entity
                             return state
 
                         if tuple(new_coords) not in pushables_coords:
-                            state[obj][i] = new_coords
-                            if tuple(new_coords) in empties_coords:
-                                state['empty'].remove(new_coords)
-                            state['empty'].append(coord)
 
+                            # update obj coordinates after move
+                            state[obj][i] = new_coords
+
+                            if tuple(new_coords) in empties_coords:
+                                state["empty"].remove(new_coords)
+
+                            state["empty"].append(coord)
+
+                        # will not change coordinate
                         elif tuple(new_coords) in pushables_coords:
                             stack = []
-                            if delta == [1, 0]:  
+
+                            if delta == [1, 0]:  # right
                                 rest_grid = 9 - coord[0]
                             elif delta == [-1, 0]:
                                 rest_grid = coord[0]
-                            elif delta == [0, 1]:  
+                            elif delta == [0, 1]:  # up
                                 rest_grid = 9 - coord[1]
                             elif delta == [0, -1]:
                                 rest_grid = coord[1]
                             else:
                                 print("ERROR IN DELTA")
 
+                            # see coordinates along direction vector
                             rest_of_coords = simulate_movement(coord, delta, grid_size=rest_grid)
                             available_space = False
                             blocked_space = False
 
                             for square in rest_of_coords:
                                 if tuple(square) in obstacle_coords:
+                                    # Cannot push into or through a 'Stop' entity
                                     blocked_space = True
                                     break
                                 elif tuple(square) in pushables_coords:
+                                    # Check if the pushable entity at this coordinate is 'Stop'
                                     pushable_entity = None
                                     for pushable in pushables:
                                         if square in state[pushable]:
                                             pushable_entity = pushable
                                             break
                                     if pushable_entity in stop_entities:
+                                        # Cannot push a 'Stop' entity
                                         blocked_space = True
                                         break
                                     else:
@@ -155,29 +187,27 @@ def transition_model(state, action):
                                 print("THERE IS SPACE FOR PUSHING")
 
                                 stack.reverse()
+
+                                # entities to be pushed
+                                print(stack)
                                 for entity_coords in stack:
                                     for pushable in pushables:
                                         if entity_coords in state[pushable]:
                                             make_push(state, pushable, entity_coords, delta)
 
+                                # controlled object will be moved to spot and push others of Stack to their new coords
                                 state[obj][i] = new_coords
-                                state['empty'].append(coord)
+                                state["empty"].append(coord)
 
+                                # Update the empty coordinates by removing the first coordinate in the stack
                                 first_in_stack = stack[0]
                                 if tuple(first_in_stack) in empties_coords:
-                                    state['empty'].remove(first_in_stack)
+                                    state["empty"].remove(first_in_stack)
                                 else:
-                                    state['empty'].append(first_in_stack)
+                                    state["empty"].append(first_in_stack)
                                     return state
+
                             else:
                                 return state
-
-    # Check for game loss condition
-    for coord in state.get('goop_obj', []):
-        if 'baba_obj' in state and coord in state['baba_obj']:
-            state['baba_obj'].remove(coord)
-            state['empty'].append(coord)
-            state['lost'] = True
-            break
 
     return state
