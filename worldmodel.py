1c30f28cce8035e17d5554f4bc675ce1e110b081
ZerghamAhmed <z@z24d>
2024-10-09 13:34:58 -0400

    add files

---
+++ worldmodel.py
@@ -0,0 +1,11 @@
+# make sure to include these import statements
+from predicates import *
+from copy import deepcopy
+from games import BabaIsYou
+from babareport import BabaReportUpdater
+from utils import directions
+
+def transition_model(state, action):
+
+    
+    return state
